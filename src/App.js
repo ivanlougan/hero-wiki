4e1b5cba97adab974618a4d42d8037e7b273fba5
Ivan Lougan <robinio123@gmail.com>
2023-02-09 14:08:10 +0000

    first message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { allHeroes } from './heroData';
 import './App.css';
 
 function App() {
+
+  const [favHeroes, setFavHeroes] = useState([]);
+
+  const handleAddToFav = (hero) => {
+    let favCopy = [...favHeroes];
+    favCopy.push(hero);
+    setFavHeroes(favCopy);
+  }
+
+  const handleRemove = (index) => {
+    let favCopy = [...favHeroes]
+    favCopy.splice(index, 1);
+    setFavHeroes(favCopy);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>hero information</h1>
+
+      <h3>FAVOURITE HEROES</h3>
+      {favHeroes.map( (favourite, index) => {
+        return <Favourites key={index} favHero={favourite} removeFunc={() => handleRemove(index)}/>
+      })}
+
+      <h3>all heroes</h3>
+
+      {allHeroes.map((heroInfo, index) => {
+        return <HeroCard key={index} heroObj={heroInfo} favFunc={handleAddToFav}/>
+      })}      
     </div>
   );
+}
+
+const Favourites = ( { favHero, removeFunc }) => {
+  return (
+    <>
+      <p>{favHero}</p>
+      <button onClick={removeFunc}>X</button>
+    </>
+  )
+}
+
+const HeroCard = (props) => {
+  const [show, setShow] = useState(false)
+
+  return (
+    <>
+      <p>HERO: {props.heroObj.hero}</p>
+
+      {show && (
+      <div>
+        <p>INFO: {props.heroObj.info}</p>
+        <p>Villain: {props.heroObj.villain}</p>
+      </div>
+      )}
+
+      <button onClick={ () => setShow( !show ) }> {show ? "HIDE INFO" : "SHOW INFO"}  </button>
+      <button onClick={ () => {props.favFunc(props.heroObj.hero)}}>Add to favourites</button>
+    </>
+  )
 }
 
 export default App;
